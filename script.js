5b4373e411edd6bdd575a00d1f901d4356987161
Ivy Wang <iwang@princeton.edu>
2022-11-03 13:41:27 -0400

    Upload starter files

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Welcome to the Lobster Club Improv Comedy Show!");
+const btn = null;
+console.log("Button?: ", btn);
+
+function getJoke() {
+    console.log("ur mom");
+}
+
+getJoke();
