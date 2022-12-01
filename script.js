73c4371309e00b4f30ac49f485c29dafb122b397
qhowery <qhowery@princeton.edu>
2022-12-01 17:32:00 -0500

    finished!

--- script.js
+++ script.js
@@ -6,4 +6,24 @@ function getJoke() {
     console.log("ur mom");
 }
 
+document.addEventListener('click', async event => {
+	if (event.target.id === 'laughButton') {
+		try {
+            const options = { 
+                method: 'GET', // specify this is a GET request, not a PUT or POST
+                headers: {
+                "Accept" : "application/json" // request the response in JSON format
+                }
+            }
+            const joke = await fetch('https://icanhazdadjoke.com/', options)
+            const jokeJson = await joke.json()
+            console.log(jokeJson)
+            document.getElementById('display').innerText = jokeJson.joke;
+        }
+        catch (error) {
+            console.err(error)
+        }
+	}
+})
+
 getJoke();
